d62bd5aca067dd5ac889c784e4b2a4cdfb88e90d
Tien Duc, Nguyen <duc_129@yahoo.de>
2021-07-03 14:50:04 +0000

    Added chair to follow player

--- Assets/Scripts/VRRig.cs
+++ Assets/Scripts/VRRig.cs
@@ -26,12 +26,13 @@ public class VRRig : MonoBehaviour
     public GameObject chair;
 
     public Transform headContraint;
-    private Vector3 headBodyOffset;
+    public Vector3 headBodyOffset;
+    public Vector3 chairOffSet;
     // Start is called before the first frame update
     void Start()
     {
         headBodyOffset = transform.position - headContraint.position;
-        //chair.transform.position = headContraint.position + headBodyOffset;
+        chairOffSet = headBodyOffset;
     }
 
     // Update is called once per frame
@@ -41,6 +42,9 @@ public class VRRig : MonoBehaviour
         transform.forward = Vector3.Lerp(transform.forward,
             Vector3.ProjectOnPlane(headContraint.forward, Vector3.up).normalized,Time.deltaTime* turnSmooth);
         //chair.transform.position = headContraint.position + headBodyOffset;
+        var old_y = chair.transform.position.y;
+        chair.transform.position = headContraint.position;// + chairOffSet;
+        chair.transform.position = new Vector3(chair.transform.position.x, old_y, chair.transform.position.z);
         chair.transform.forward = Vector3.Lerp(chair.transform.forward,
             Vector3.ProjectOnPlane(headContraint.forward, Vector3.up).normalized, Time.deltaTime * turnSmooth);
         head.Map();
